64a289c5eac2c434813be669d19d00ebac667c3f
AlexF99 <alexandreopf@gmail.com>
2020-05-14 22:09:23 -0300

    fixed issue with repeated operators

--- index.js
+++ index.js
@@ -6,21 +6,38 @@ const clearAll = document.querySelector('.clear-all');
 
 let statement;
 
+function actionOperations(action){
+  if (action == 'disable'){
+    operation.forEach((item, i) => {
+      item.setAttribute("disabled", true);
+    });
+  }
+  if (action == 'remove'){
+    operation.forEach((item, i) => {
+      item.removeAttribute("disabled");
+    });
+  }
+}
+
 numbers.forEach((item, i) => {
   item.addEventListener('click', () => {
     screenCalc.innerHTML += item.value;
+    // actionOperations('remove');
   })
 });
 operation.forEach((item, i) => {
   item.addEventListener('click', () => {
     screenCalc.innerHTML += item.value;
+    actionOperations('disable');
   })
 });
 equals.addEventListener('click', () => {
   statement = screenCalc.innerHTML;
   screenCalc.innerHTML = eval(statement);
+  actionOperations('remove');
 })
 clearAll.addEventListener('click', () => {
   statement = '';
   screenCalc.innerHTML = '';
+  actionOperations('remove');
 })
